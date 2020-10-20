1adaeb6c2097e821ca68cf349fa7165f4c41f635
Menno <mennobil02@gmail.com>
2020-10-20 09:17:39 +0200

    [FEATURE] test skelet

---
+++ AI_ChatApp2.0_Test/BotTester.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AI_ChatApp2._0_Test
+{
+    [TestClass]
+    public class BotTester
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
---
+++ AI_ChatApp2.0_Tests/BotTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AI_ChatApp2._0_Tests
+{
+    [TestClass()]
+    class BotTests
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        [TestMethod()]
+        public void FibonacciTestValid()
+        {
+            
+        }
+    }
+}
